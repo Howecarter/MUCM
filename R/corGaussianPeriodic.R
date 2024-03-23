37b106089363c7976547e3accf280ecd1166bf1d
Howecarter <112195873+Howecarter@users.noreply.github.com>
2024-03-22 23:10:04 -0700

    Update corGaussianPeriodic.R

--- R/corGaussianPeriodic.R
+++ R/corGaussianPeriodic.R
@@ -82,6 +82,7 @@ corGaussianPeriodicSquare2 <- function(inputs, phi, period) {
     }
     
     for (i in 1:n.inputs) {
+        i
         dist.mat <- rdist(inputs[, i])
         stopifnot(dist.mat >= 0, dist.mat <= period[i])
         min.dist <- pmin(period[i] - dist.mat, dist.mat)
